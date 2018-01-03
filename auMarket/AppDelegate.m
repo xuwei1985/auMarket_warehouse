44984391030d4448f6229dfdde5544014afdf29c
vivi <172498667@qq.com>
2018-01-03 23:29:27 +0800

    00

--- auMarket/AppDelegate.m
+++ auMarket/AppDelegate.m
@@ -84,7 +84,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     [[NSNotificationCenter defaultCenter] postNotificationName:APP_WILL_ENTER_FOREGROUND object:nil];
-    [self.booter loadTaskList];
 }
 
 
