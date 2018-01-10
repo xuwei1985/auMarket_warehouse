a43e4eafbbba3245757a5c663ccf66486ee94d5b
吴绪伟 <172498667@qq.com>
2018-01-10 18:55:28 +0800

    daily update

---
+++ auMarket/Sections/StockCell.h
@@ -0,0 +1,29 @@
+//
+//  MemberCell.h
+//  XiaoYa
+//
+//  Created by XuWei on 15/1/3.
+//  Copyright (c) 2015年 xuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BatchEntity.h"
+
+typedef void(^AddGoodsBlock)(NSString *batch_id);
+
+@interface StockCell : UITableViewCell
+{
+    UIImageView *imv_addGoods;
+    UILabel *lbl_batchNo_prefix;
+    UILabel *lbl_batchNo;
+    UILabel *lbl_suppliersName_prefix;
+    UILabel *lbl_suppliersName;
+    UILabel *lbl_editTime_prefix;
+    UILabel *lbl_editTime;
+}
+
+@property(nonatomic,retain) BatchItemEntity *entity;
+@property (nonatomic, copy) AddGoodsBlock addGoodsBlock;
+
+-(void)returnBatchId:(AddGoodsBlock)block;
+@end
