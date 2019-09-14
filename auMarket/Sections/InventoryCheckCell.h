b3e93f6adff144127846bc87ddd3a1eee315d0fb
吴绪伟 <172498667@qq.com>
2019-09-14 23:32:26 +0800

    no message

---
+++ auMarket/Sections/InventoryCheckCell.h
@@ -0,0 +1,31 @@
+//
+//  PickOrderCell.h
+//  auMarket
+//
+//  Created by 吴绪伟 on 2018/5/21.
+//  Copyright © 2018 daao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ShelfEntity.h"
+
+typedef void(^AddStackBlock)(ShelfItemEntity *entity);
+@interface InventoryCheckCell : UITableViewCell
+{
+    UIButton *btn_select;
+    UILabel *lbl_order_sn;
+    UILabel *lbl_order_goods_num;
+    UILabel *lbl_order_region;
+    UILabel *lbl_order_price;
+    UILabel *lbl_bind_tip;
+    UILabel *lbl_bind_mark;
+    
+    UILabel *lbl_order_sn_value;
+    UILabel *lbl_order_goods_num_value;
+    UILabel *lbl_order_region_value;
+    UILabel *lbl_order_price_value;
+}
+@property(nonatomic,retain) ShelfItemEntity *entity;
+@property (nonatomic, copy) AddStackBlock addStackBlock;
+-(void)addStack:(AddStackBlock)block;
+@end
