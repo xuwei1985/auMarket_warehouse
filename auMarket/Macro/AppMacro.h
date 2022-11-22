c40d098117ae8fb6bfa9835014dea29cc4d2fc86
wuxuwei <172498667@qq.com>
2022-11-22 23:56:03 +0800

    no message

--- auMarket/Macro/AppMacro.h
+++ auMarket/Macro/AppMacro.h
@@ -27,7 +27,7 @@ return __singleton__; \
 
 //接口相关
 #ifdef DEBUG//测试
-    #define SERVER_ADDRESS        @"http://api2_test.bigau.com"
+    #define SERVER_ADDRESS        @"http://api2.bigau.com"
 
 #else//正式
     #define SERVER_ADDRESS          @"http://api2.bigau.com"
